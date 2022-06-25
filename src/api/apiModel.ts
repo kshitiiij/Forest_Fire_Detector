1e0172f06be064c61945e9d24c3b6514be11754c
Extreme <extreme@pop-os.localdomain>
2022-06-25 22:00:19 +0530

    predictApi added

---
+++ src/api/apiModel.ts
@@ -0,0 +1,29 @@
+const API_BASE_URL = "localhost:8000/api/"
+
+type RequestMethod = 'POST'
+type RequestBody = {
+    url: string
+}
+
+const request = async (endpoint: string, method:RequestMethod = 'POST', data:any = {})=>{
+
+    const response = await fetch(API_BASE_URL + endpoint, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+
+    if(response.ok) {
+        const json = await response.json();
+        return json;
+    } else {
+        const errorJson = await response.json()
+        throw Error(errorJson);
+    }
+} 
+
+export const predictClass = (data:RequestBody)=> {
+    return request('predict/', 'POST', data)
+}
